bb503e9b2badf7156d70d36c72f71b495d68743e
Aman Akhodiya <ak75202751@gmail.com>
2025-03-01 18:35:12 +0530

    fix heart icon for product

--- addons/custom_product_page/controllers/product_apis.py
+++ addons/custom_product_page/controllers/product_apis.py
@@ -17,7 +17,7 @@ class ProductAPIController(http.Controller):
 
     #     return user
 
-    @http.route('/api/products', auth='none', methods=['GET'], type='json')
+    @http.route('/api/products', auth='public', methods=['GET'], type='http')
     def get_products(self, **kwargs):
         # user = self._authenticate()
         # if isinstance(user, dict):  # Authentication failed
@@ -31,13 +31,13 @@ class ProductAPIController(http.Controller):
         } for product in products]
         return product_list
     
-        @http.route('/api/product/<int:product_id>', auth='public', methods=['GET'], type='json')
-        def get_product(self, product_id, **kwargs):
-            product = request.env['product.template'].sudo().browse(product_id)
-            if not product.exists():
-                return {'error': 'Product not found'}
-            return {
-                'id': product.id,
-                'name': product.name,
-                'price': product.list_price
-            }
+    @http.route('/api/product/<int:product_id>', auth='public', methods=['GET'], type='json')
+    def get_product(self, product_id, **kwargs):
+        product = request.env['product.template'].sudo().browse(product_id)
+        if not product.exists():
+            return {'error': 'Product not found'}
+        return {
+            'id': product.id,
+            'name': product.name,
+            'price': product.list_price
+        }
